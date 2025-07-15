3e312040f1951904841a25ffa1875169e26e00fb
Tauheed-Esc <mohammedabdultauheed@hotmail.com>
2025-07-15 18:49:50 +0530

    modified user routes to verify authentication of a user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const {
+  createUser,
+  verifyOtp,
+  loginUser
+} = require('../controllers/userController');
 
-// CRUD Routes
-router.post('/', userController.createUser);       // Create
-router.get('/', userController.getAllUsers);       // Read all
-router.get('/:id', userController.getUserById);    // Read one
-router.put('/:id', userController.updateUser);     // Update
-router.delete('/:id', userController.deleteUser);  // Delete
+router.post('/create', createUser);
+router.post('/verify-otp', verifyOtp);
+router.post('/login', loginUser);
 
 module.exports = router;

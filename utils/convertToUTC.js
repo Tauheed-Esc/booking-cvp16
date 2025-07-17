82643e28aacdd328e85d54c2af549121bb3a0a0d
Tauheed-Esc <mohammedabdultauheed@hotmail.com>
2025-07-17 14:51:51 +0530

    changed dob formate to 'DD-MM-YYYY'

--- utils/convertToUTC.js
+++ utils/convertToUTC.js
@@ -1,4 +1,9 @@
 module.exports = function convertToUTCFromIST(dateStr) {
-  const istDate = new Date(`${dateStr}T00:00:00+05:30`);
+  // Expecting dateStr in 'DD-MM-YYYY' format
+  const [day, month, year] = dateStr.split('-').map(Number);
+  // Create date in IST timezone
+  const istDate = new Date(Date.UTC(year, month - 1, day, 0, 0, 0));
+  // Adjust for IST offset (+5:30)
+  istDate.setUTCHours(istDate.getUTCHours() - 5, istDate.getUTCMinutes() - 30);
   return new Date(istDate.toUTCString());
 }
